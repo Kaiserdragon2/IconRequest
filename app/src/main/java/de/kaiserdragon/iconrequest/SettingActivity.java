9bc6ea7babaccbf6c9b508f4cc7c2705951f6bd0
Bastian <8929967+Kaiserdragon2@users.noreply.github.com>
2024-10-13 20:36:07 +0200

    Layout updates

--- app/src/main/java/de/kaiserdragon/iconrequest/SettingActivity.java
+++ app/src/main/java/de/kaiserdragon/iconrequest/SettingActivity.java
@@ -1,11 +1,12 @@
 package de.kaiserdragon.iconrequest;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,26 +18,21 @@ import java.util.Objects;
 import de.kaiserdragon.iconrequest.helper.SettingsHelper;
 
 public class SettingActivity extends AppCompatActivity {
-
+    Button setDark;
+    Button setLight;
+    Button setDefault;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
+        setDark = findViewById(R.id.ButtonDarkMode);
+        setLight = findViewById(R.id.ButtonLightMode);
+        setDefault = findViewById(R.id.ButtonSystem);
+        painThemeButton();
         TextView PolicyView = findViewById(R.id.PrivacyPolicy);
         PolicyView.setMovementMethod(LinkMovementMethod.getInstance());
 
-        switch (SettingsHelper.loadData("DarkModeState", this)) {
-            case -1:
-                ((RadioGroup) findViewById(R.id.RadioTheme)).check(R.id.radioDefault);
-                break;
-            case 1:
-                ((RadioGroup) findViewById(R.id.RadioTheme)).check(R.id.radioLight);
-                break;
-            case 2:
-                ((RadioGroup) findViewById(R.id.RadioTheme)).check(R.id.radioDark);
-                break;
-        }
 
         ((CheckBox) findViewById(R.id.checkBoxRows)).setChecked(SettingsHelper.loadDataBool("SettingRow", this));
         ((CheckBox) findViewById(R.id.checkBoxOnly)).setChecked(SettingsHelper.loadDataBool("SettingOnlyNew", this));
@@ -44,13 +40,13 @@ public class SettingActivity extends AppCompatActivity {
         ((CheckBox) findViewById(R.id.checkActionMain)).setChecked(SettingsHelper.loadDataBool("ActionMain", this));
 
 
-        Button setDark = findViewById(R.id.radioDark);
+        //Button setDark = findViewById(R.id.radioDark);
         setDark.setOnClickListener(view -> start(view, 2));
 
-        Button setLight = findViewById(R.id.radioLight);
+        //Button setLight = findViewById(R.id.radioLight);
         setLight.setOnClickListener(view -> start(view, 1));
 
-        Button setDefault = findViewById(R.id.radioDefault);
+        //Button setDefault = findViewById(R.id.radioDefault);
         setDefault.setOnClickListener(view -> start(view, -1));
 
         CheckBox SecondRow = findViewById(R.id.checkBoxRows);
@@ -66,13 +62,17 @@ public class SettingActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.settings);
     }
 
     public void start(View view, int update) {
         if (update != 0) {
             SettingsHelper.saveData("DarkModeState", update, this);
+            Log.d("Update", String.valueOf(update));
             AppCompatDelegate.setDefaultNightMode(update);
+            painThemeButton();
         } else {
             if (view == findViewById(R.id.checkBoxRows)) {
                 SettingsHelper.saveDataBool("SettingRow", ((CheckBox) view).isChecked(), this);
@@ -85,6 +85,27 @@ public class SettingActivity extends AppCompatActivity {
             }
         }
 
+    }
+    public void painThemeButton(){
+        resetButtonColors();
+        switch (SettingsHelper.loadData("DarkModeState", this)) {
+            case -1:
+                setDefault.setBackgroundColor(Color.BLUE);
+                break;
+            case 1:
+                setLight.setBackgroundColor(Color.BLUE);
+                break;
+            case 2:
+                setDark.setBackgroundColor(Color.BLUE);
+                break;
+        }
+
+    }
+    // Method to reset all button colors to default
+    private void resetButtonColors() {
+        setDefault.setBackgroundColor(getResources().getColor(R.color.primary));
+        setDark.setBackgroundColor(getResources().getColor(R.color.primary));
+        setLight.setBackgroundColor(getResources().getColor(R.color.primary));
     }
 }
 
